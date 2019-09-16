8c3847032a4a88e9f3a69e96e498432c82228ff4
jungwan <jungwan82@naver.com>
2019-09-16 22:17:44 +0900

    rand

--- Utility.h
+++ Utility.h
@@ -96,4 +96,19 @@ namespace fb {
 		// aligned value = 512
 		return (value + (alignment-1)) & ~(alignment-1);
 	}
+
+	inline int Rand(int a, int b)
+	{
+		return a + rand() % ((b - a) + 1);
+	}
+
+	inline float RandF()
+	{
+		return (float)(rand()) / (float)RAND_MAX;
+	}
+
+	inline float RandF(float a, float b)
+	{
+		return a + RandF() * (b - a);
+	}
 }
